f40f716f01f3aad0e2a59d585403034e0102cdd4
Alapottra <alapottratrojon@gmail.com>
2022-12-31 12:25:30 +0600

    first commit

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,28 @@
+import { actionTypes } from "../actionTypes/actionTypes";
+
+const initialState = {
+    cart: []
+};
+
+
+const productReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.ADD_TO_CART:
+            return {
+                ...state,
+                cart: [...state.cart, action.payload]
+
+            };
+        
+        case actionTypes.REMOVE_FROM_CART: {
+            return{
+                ...state
+            }
+        }    
+        default:
+            return state;
+    }
+
+}
+
+export default productReducer;
